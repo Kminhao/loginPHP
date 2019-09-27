638fcc2f9a3f14198effe8a3abec00ced4fd9515
Bruno Adriano <brunoa.f@hotmail.com>
2019-09-27 08:26:41 -0300

    formularios de cadastro e login

--- validacao.php
+++ validacao.php
@@ -2,8 +2,11 @@
     $btnLogin = filter_input(INPUT_POST, 'btnLogin', FILTER_SANITIZE_STRING);
 
     if($btnLogin){
-
+        $usuario = filter_input(INPUT_POST, 'email', 'FILTER_SANITIZE_STRING');
+        $senha = filter_input(INPUT_POST, 'senha', 'FILTER_SANITIZE_STRING');
+        echo $usuario - $senha;
     }else{
-        $_SESSION['msg']
+        $_SESSION['msg'] = "Página não encontrada";
+        header("Location: index.html");
     }
 ?>
