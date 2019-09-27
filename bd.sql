8d3b706232c51b64931cfeb300c6a11ce3b1d4ac
Bruno Adriano <brunoa.f@hotmail.com>
2019-09-27 08:20:48 -0300

    iniciando projeto

---
+++ bd.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS seg;
+CREATE DATABASE seg;
+
+CREATE TABLE seg.usuario(
+    id int not null auto_increment primary key,
+    email varchar(256) not null UNIQUE,
+    senha varchar(256) not null,
+    nome varchar(256) not null,
+    data_nascimento date not null,
+    created_at date not null,
+    deleted_at date
+);
---
+++ validacao.php
@@ -0,0 +1,9 @@
+<?php
+    $btnLogin = filter_input(INPUT_POST, 'btnLogin', FILTER_SANITIZE_STRING);
+
+    if($btnLogin){
+
+    }else{
+        $_SESSION['msg']
+    }
+?>
